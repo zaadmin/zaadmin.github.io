09604365cda2d112ecdbdb469e4e5eee2b69e8df
zhangai <ai.zhang@usmarthk.com>
2022-08-17 17:03:00 +0800

    调整目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,16 +2,31 @@
  * @Description: 
  * @Autor: zhangai
  * @Date: 2022-08-17 10:30:06
- * @LastEditTime: 2022-08-17 10:33:54
+ * @LastEditTime: 2022-08-17 16:58:26
  */
 module.exports = {
     lang: 'zh-CN',
     title: 'front-end',
     description: '前端体系',
-    base:'/front-end-learning/',
+    base:'/web-system-coding/',
     // head: [['link', { rel: 'icon', href: '/node-learning-ebook/favicon.ico' }]],
     theme: '@vuepress/theme-default',
     themeConfig: {
         displayAllHeaders:true,
+        navbar:  [
+            { text: '首页', link: '/' },
+            { text: '小结', link: '/md/HTTP/http'}
+        ],
+        sidebar: {
+            '/md/HTTP/': [
+            {
+              text: '',
+              collapsible:false,
+              children: [
+                '/md/HTTP/http.md',
+                '/md/HTTP/TCP-IP.md',
+              ],
+            },]
+        }
     }
 }
